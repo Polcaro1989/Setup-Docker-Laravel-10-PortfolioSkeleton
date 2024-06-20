b86446b48bedaade96b98b10637576d03f6ad232
abraao69 <fulllstack695@gmail.com>
2024-06-20 16:54:12 -0300

    first commit

---
+++ resources/views/day/components/footer.blade.php
@@ -0,0 +1,72 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-info">
+                        <h3>Polcaro</h3>
+                        <p>
+                            Rio de Janeiro<br><br>
+                            <strong>Telefone:</strong> (21)9950-18935<br>
+                            <strong>Email:</strong>abraao695@gmail.com<br>
+                        </p>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Links Úteis</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Sobre</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Serviços</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Termos de serviço</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Política de Privacidade</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Nossos Serviços</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Designer de Web</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Desenvolvimento web</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Gestão de produtos</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Design gráfico</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Nosso boletim informativo</h4>
+                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Se inscrever">
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>Polcaro</span></strong>. Todos os direitos reservados
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/day-multipurpose-html-template-for-free/ -->
+          Creditos:<a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
